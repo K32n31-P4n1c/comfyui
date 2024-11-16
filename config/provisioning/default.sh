a38cd40d6b3eac3e41f80e8894e3fcfb015139f2
Andrej <Andrej.Burovski@protonmail.com>
2024-11-16 16:36:41 +0100

    Update default.sh

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -21,6 +21,8 @@ PIP_PACKAGES=(
 NODES=(
     "https://github.com/ltdrdata/ComfyUI-Manager"
     "https://github.com/cubiq/ComfyUI_essentials"
+    "https://github.com/rgthree/rgthree-comfy"
+    "https://github.com/jags111/efficiency-nodes-comfyui"
 )
 
 CHECKPOINT_MODELS=(
