65384a83ad18396bfbf519354df0b2ba544e9467
Andrej <Andrej.Burovski@protonmail.com>
2024-12-15 04:43:50 +0100

    Update flux.sh

--- config/provisioning/flux.sh
+++ config/provisioning/flux.sh
@@ -28,8 +28,6 @@ NODES=(
 )
 
 CHECKPOINT_MODELS=(
-    #FLUX
-    "https://civitai.com/api/download/models/691639?type=Model&format=SafeTensor&size=pruned&fp=fp8&token=c1a3270f7ed25469dffaced28e6e2b5d"
 )
 
 CLIP_MODELS=(
@@ -38,6 +36,8 @@ CLIP_MODELS=(
 )
 
 UNET_MODELS=(
+    #FLUX
+    "https://civitai.com/api/download/models/691639?type=Model&format=SafeTensor&size=pruned&fp=fp8&token=c1a3270f7ed25469dffaced28e6e2b5d"
 )
 
 VAE_MODELS=(
