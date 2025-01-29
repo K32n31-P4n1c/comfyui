3c680f7d2d3e0a9cce4abf440f2974db6644a256
Andrej <Andrej.Burovski@protonmail.com>
2025-01-29 01:47:34 +0100

    Update Hunyuan3D-2.sh

--- config/provisioning/Hunyuan3D-2.sh
+++ config/provisioning/Hunyuan3D-2.sh
@@ -7,7 +7,20 @@
 ###################################
 # 1. Define which apt packages you want:
 ###################################
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends wget
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+
+wget https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda-repo-ubuntu2204-12-1-local_12.1.1-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-1-local_12.1.1-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+
+# 2. Then define your APT_PACKAGES array, including build-essential + CUDA dev:
 APT_PACKAGES=(
+    "build-essential"
+    "cuda-toolkit-12-1"
 )
 
 PIP_PACKAGES=(
